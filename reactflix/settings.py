febe71244bcbeefe06a394b23287e14f5e4d7f53
Davi F <davi.felix.lima@gmail.com>
2021-11-23 00:04:28 -0300

    Add migrations to repo

--- reactflix/settings.py
+++ reactflix/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'reactflix',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     },
@@ -150,3 +150,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
